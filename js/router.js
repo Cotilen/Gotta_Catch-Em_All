ba3b272b0ad5daa1c5405ed865658fc0131f4a98
Cleiton <cleitonncruz105@gmail.com>
2023-05-02 08:47:20 -0300

    Arrumado

--- js/router.js
+++ js/router.js
@@ -1,31 +1,34 @@
-'use strict'
+"use strict";
+
+import './app.js'
+import {pokedex} from './app.js'
+
 
 const routes = {
-    '/home': '/pages/home.js',
-    '/pokedex': '/pages/pokedex.js',
-    '/games': '/pages/games.html',
-    '/regions': '/pages/regions.html'
+  "/": "/pages/home.html",
+  "/pokedex": "/pages/pokedex.html"
 }
 
-const route = () => {
-    window.event.preventDefault()
-    window.history.pushState({}, "", window.event.target.href)
-    handleLocation()
-}
+const route = async () => {
+  window.event.preventDefault();
+  window.history.pushState({}, "", window.event.target.href);
+  const path = window.location.pathname;
+  const route = routes[path];
 
-const handleLocation = async() => {
+  const response = await fetch(route);
+  const html = await response.text();
 
-    const path = window.location.pathname
-    const route = await routes[path]
-    const { page } = await
-    import (route);
+  document.getElementById("root").innerHTML = html;
 
-    console.log(page())
+  if (path == '/pokedex') {
+    console.log('pokedex');
+    pokedex()
+  } else if (path == '/') {
+    console.log('home');
+  }
 
-    document.getElementById('root').replaceChildren(page())
-}
+};
 
-window.onpopstate = handleLocation
-window.route = route
 
-handleLocation()
+
+window.route = route;
--- pages/home.js
+++
@@ -1,7 +0,0 @@
-export function page() {
-
-    const pikachu = document.createElement('img')
-    pikachu.src = '../images/pikachu.png'
-
-    return pikachu
-}
